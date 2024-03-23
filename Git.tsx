73ec8fafa951fc00235ad1a629e7012efbd2840e
gcyogesh <gcyogesh962@gmail.com>
2024-03-24 03:24:18 +0545

    git

---
+++ Git.tsx
@@ -0,0 +1,36 @@
+console.log("k xa LANTAHYY")       
+
+
+
+// to see what changes we made in git IT WILL SHOW ALL COMMIT YOU DID = git log   
+// FOR CHECKOUT WHAT COMMIT YOU DID = git CHECKOUT (YOUR COMMIT ID)   TO GO TO MAIN branch (git checkout master);
+
+// BRANCHES 
+// git branch "branch-name"    // TO MAKE branch;
+// .git branch    // TO SEE NUMBER OF branch;
+// TO SWITCH INTO ANOTHER branch = git checkout "branch-name";
+
+// TO MERGE branchES  = git merge <YOUR ANOTHER branch>
+// ANOTHER WAY OF SWITCHING git branch  = git switch <branchNAME>
+
+// TO CREATE, SWITCH , CHECKOUT branch AT ONCE = git checkout -B <NEW-branch>
+
+// DELETE THE branch = git branch -d <branch-NAME> (SMALL d)
+
+// IF YOU HAVE DATA INSIDE THAT branch AND YOU HAVE TO DELETE IT THEN = git branch -D <branch-NAME>  (CAPITAL D)
+
+// TO REMOVE TRACKED FILES = git rm <FILE-NAME>   (if i alreay commit to git and i deleted from my folder we do this)
+
+//TO KNOW THE FILES WE HAVE IN GIT =   git ls-files
+
+//HOW TO HANDLE CHANGES WHICH WE ARE NOT TEND TO BE CHANGED = git restore --staged <file-name>   (Example: we did changes in gitignore file whilch
+// is not needed and it's already git commited)
+
+// WHAT IF YOU COMMITED THE WORK IN YOUR REPO AND MUST DELETE IT(THE LAST COMMIT) = git reset HEAD~1;       (NW)
+
+// WHEN YOUR WORK IS NOT COMPLETED YET AND YOU DON'T WANT TO COMMIT THEM  = git stash
+// IF YOU ARE RETURN FOR WORK AGAIN THEN = git stash apply
+
+
+
+
